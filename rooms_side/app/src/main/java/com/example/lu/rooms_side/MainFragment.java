ee6f421a05a2ba0d75a9cd2cd73a5f2d5490c0fa
Maxwell Lu <maxwell.mark.lu@Live.com>
2014-10-26 03:29:32 -0500

    User Interface commit

---
+++ rooms_side/app/src/main/java/com/example/lu/rooms_side/MainFragment.java
@@ -0,0 +1,31 @@
+package com.example.lu.rooms_side;
+
+import android.app.Activity;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Lu on 2014/10/26.
+ */
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
